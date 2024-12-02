7c8b0f9a136372697c66a92a3497387ef2e0d452
aleja678 <aleja_naranjo_678@hotmail.com>
2024-12-02 21:49:18 +0100

    Initial commit

---
+++ images_galery/src/test/java/com/personal_project/images_galery/ImagesGaleryApplicationTests.java
@@ -0,0 +1,16 @@
+/*
+package com.personal_project.images_galery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesGaleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test para verificar que el contexto se carga correctamente
+	}
+
+}
+*/
